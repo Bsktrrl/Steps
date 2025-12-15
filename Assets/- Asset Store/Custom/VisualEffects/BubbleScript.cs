0544cdfff68f217d01298e1b2293406a5876ab3c
Adrian Klausen <68854734+Bsktrrl@users.noreply.github.com>
2025-12-15 20:06:37 +0100

    Lava Bouble speed

--- Assets/- Asset Store/Custom/VisualEffects/BubbleScript.cs
+++ Assets/- Asset Store/Custom/VisualEffects/BubbleScript.cs
@@ -6,11 +6,13 @@ public class BubbleScript : MonoBehaviour
 {
     Animator anim;
 
+    [SerializeField] float newSpeed = 0.5f;
     [SerializeField] float rangeMax = 0.5f;
 
     void Start()
     {
         anim = GetComponent<Animator>();
+        anim.speed = newSpeed;
         anim.SetFloat("CycleOffset", Random.Range(0f, rangeMax));
     }
 }
