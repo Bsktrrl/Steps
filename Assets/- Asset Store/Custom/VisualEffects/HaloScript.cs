378e9230573d051dae846e4375627667ff6cbb24
Mathias Kapelrud <mathias.kapelrud@gmail.com>
2026-07-06 15:28:45 +0200

    Halo
    - Made halo material appear through walls, but slightly more transparent
    - Made hint beam's halo not disappear behind walls

--- Assets/- Asset Store/Custom/VisualEffects/HaloScript.cs
+++ Assets/- Asset Store/Custom/VisualEffects/HaloScript.cs
@@ -6,24 +6,37 @@ using UnityEngine.Windows.Speech;
 public class HaloScript : MonoBehaviour
 {
     [SerializeField] float scale;
+    [SerializeField] bool ignoreOpaques;
     float minDistance = 3f;
     float speed = 10f;
     float size;
 
     void Update()
     {
-        if (Physics.Raycast(transform.position, Camera.main.transform.position - transform.position, Vector3.Distance(transform.position, Camera.main.transform.position)))
+        if (ignoreOpaques)
         {
-            size = 0.1f;
+            CalculateSize();
         }
         else
         {
-            size = Vector3.Distance(transform.position, Camera.main.transform.position) / 2;
-            size = Mathf.Max(size, minDistance) / minDistance;
-            size *= scale;
-            size = Mathf.Max(size, scale);
+            if (Physics.Raycast(transform.position, Camera.main.transform.position - transform.position, Vector3.Distance(transform.position, Camera.main.transform.position)))
+            {
+                size = 0f;
+            }
+            else
+            {
+                CalculateSize();
+            }
         }
 
         transform.localScale = Vector3.one * Mathf.Lerp(transform.localScale.x, size, speed * Time.deltaTime);
+    }
+
+    void CalculateSize()
+    {
+        size = Vector3.Distance(transform.position, Camera.main.transform.position) / 2;
+        size = Mathf.Max(size, minDistance) / minDistance;
+        size *= scale;
+        size = Mathf.Max(size, scale);
     }
 }
