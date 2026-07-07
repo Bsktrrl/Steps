91bff9caccbc901776c9eb57c821e4eb7225c520
Mathias Kapelrud <mathias.kapelrud@gmail.com>
2026-07-07 21:02:47 +0200

    Fog
    - Added a bunch of features to fog volume

--- Assets/- Asset Store/Custom/VisualEffects/FogVolume.cs
+++ Assets/- Asset Store/Custom/VisualEffects/FogVolume.cs
@@ -4,39 +4,48 @@ using System.Drawing;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-public class FogScript : MonoBehaviour
+public class FogVolume : MonoBehaviour
 {
+    Camera cam;
+
     bool inside;
     bool runOnce;
 
-    float edgePadding = 0.1f;
-    float depthMin = 0.5f;
+    float depth;
+    float lastDepth;
+
+    float edgePadding = 0.25f;
+    float depthMin = 0f;
     float depthMax = 0.75f;
 
     void Start()
     {
         Shader.SetGlobalFloat("_FogStrength", 0);
+
+        cam = Camera.main;
     }
 
     void Update()
     {
-        Vector3 point = transform.InverseTransformPoint(Camera.main.transform.position);
+        Vector3 point = transform.InverseTransformPoint(cam.transform.position);
         Vector3 halfSize = Vector3.one * 0.5f + Vector3.one * edgePadding;
 
         Vector3 distance = new Vector3(Mathf.Abs(point.x), Mathf.Abs(point.y), Mathf.Abs(point.z));
 
-        float depth = Mathf.Min(halfSize.x - distance.x, Mathf.Min(halfSize.y - distance.y, halfSize.z - distance.z));
+        depth = Mathf.Min(halfSize.x - distance.x, Mathf.Min(halfSize.y - distance.y, halfSize.z - distance.z));
 
-        print(depth);
-
-        inside = depth > 0 ? true : false;
+        inside = depth > 0;
 
         depth = Mathf.Clamp(depth, depthMin, depthMax);
 
         if (inside)
         {
-            Shader.SetGlobalFloat("_FogStrength", depth);
+            if (depth != lastDepth)
+            {
+                Shader.SetGlobalFloat("_FogStrength", depth);
 
+                lastDepth = depth;
+            }
             runOnce = true;
         }
         else
